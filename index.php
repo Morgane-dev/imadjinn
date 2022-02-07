fdfb529a75c41597b1b81d0703a7cca0f99ada32
Morgane-dev <99172396+Morgane-dev@users.noreply.github.com>
2022-02-07 10:55:50 +0100

    téléchargement manuel des fichiers

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Imadjinn Bibliothèque</title>
+</head>
+<body>  
+    <header>
+        <div id="menu200px">
+            <div id="logo_div">
+                <p>bibliothèque</p>
+                <a href = "#" id="logo">Imadjinn</a>
+            </div>
+
+            <div id="burger" class="">
+                <span>
+                </span>
+            </div>
+
+            <div id = "secret-menu" class="">
+                <p></p>
+                <ul>
+                    <li>Parcourir les livres par :</li>
+                    <li><a href="#">Genres</a></li>
+                    <li><a href="#">Auteurs</a></li>
+                    <li id="contact"><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            
+        </div>
+        <div id="connexion">
+            <a href="#">
+                <img src="img/icones/avatar.png" alt="icône d'avatar de connexion">
+                <p>Connexion</p>
+            </a>
+        </div>
+        
+
+        <div id="search_bar">
+            <button type="button" id="filter_btn">Filtres</button>
+            <input type="search" name="search_input" placeholder= "Recherchez votre livre ici..." id="search_input">
+            <button type="submit" id="search_btn"><img src="img/icones/magnifier.png" alt="icône de loupe"></button>
+        </div>
+    </header>
+
+    <section id="section_recommand">
+        <div class="padding10" id=recommandations>
+            <h2>Nos Recommandations</h2>
+            <p class="center">Les coups de coeurs du staff Imadjinn</p>
+            <div>
+                <img src="img/livres/les-sirenes-de-gotham.jpg" alt="">
+                <h3>Les sirènes de Gotham</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ullam error repellat laborum minus accusantium, asperiores, officia sed corrupti modi fugit incidunt illum sint veritatis ea cumque dolorem, aperiam laboriosam.</p>
+            </div>
+            <div>
+                <img src="img/livres/memoires_dune_geisha.jpg" alt="">
+                <h3>Mémoire d'une geisha</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ullam error repellat laborum minus accusantium, asperiores, officia sed corrupti modi fugit incidunt illum sint veritatis ea cumque dolorem, aperiam laboriosam.</p>
+            </div>
+            <div>
+                <img src="img/livres/gastronogeek.jpg" alt="">
+                <h3>Gastronogeek</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ullam error repellat laborum minus accusantium, asperiores, officia sed corrupti modi fugit incidunt illum sint veritatis ea cumque dolorem, aperiam laboriosam.</p>
+            </div>
+            <div>
+                <img src="img/livres/orgueil_et_prejuges.jpg" alt="">
+                <h3>Orgueil et préjugés</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ullam error repellat laborum minus accusantium, asperiores, officia sed corrupti modi fugit incidunt illum sint veritatis ea cumque dolorem, aperiam laboriosam.</p>
+            </div>
+            <div>
+                <img src="img/livres/traumatiser_son_enfant.jpg" alt="">
+                <h3>Traumatiser son enfant pour en faire un être inadapté mais génial</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ullam error repellat laborum minus accusantium, asperiores, officia sed corrupti modi fugit incidunt illum sint veritatis ea cumque dolorem, aperiam laboriosam.</p>
+            </div>
+            <div>
+                <img src="img/livres/mes_belles_etoiles.jpg" alt="">
+                <h3>Mes belles étoiles</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ullam error repellat laborum minus accusantium, asperiores, officia sed corrupti modi fugit incidunt illum sint veritatis ea cumque dolorem, aperiam laboriosam.</p>
+            </div>
+        </div>
+
+    </section>
+
+    <section>
+        <div>
+            <h2>Nos recommandations</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, odio beatae? Vitae est optio tenetur obcaecati architecto, deserunt nihil recusandae harum nam fugiat eligendi animi debitis repellendus quibusdam tempora odio.</p>
+        </div>
+        <div></div>
+    </section>
+
+    <section>
+        <div>
+            <h2>Découverte d'un livre au hasard</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, odio beatae? Vitae est optio tenetur obcaecati architecto, deserunt nihil recusandae harum nam fugiat eligendi animi debitis repellendus quibusdam tempora odio.</p>
+        </div>
+        <div></div>
+    </section>
+
+    <section>
+        <div>
+            <h2>Titre de section</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, odio beatae? Vitae est optio tenetur obcaecati architecto, deserunt nihil recusandae harum nam fugiat eligendi animi debitis repellendus quibusdam tempora odio.</p>
+        </div>
+        <div></div>
+    </section>
+
+    <section>
+        <div>
+            <h2>Titre de section</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, odio beatae? Vitae est optio tenetur obcaecati architecto, deserunt nihil recusandae harum nam fugiat eligendi animi debitis repellendus quibusdam tempora odio.</p>
+        </div>
+        <div></div>
+    </section>
+    <div></div>
+
+
+    <footer></footer>
+    <script src="main.js"></script>
+</body>
+</html>
+
+
+<!-- utilisateur peut switch sur le thème sombre -->
